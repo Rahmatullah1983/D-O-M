ed6e915415a2d42eae6f4e2ed6ef7048e94185be
Rahmatullah1983 <bugtirahmatullah@gmail.com>
2023-08-06 14:07:19 +0500

    today I learnt basic selectors of dom

---
+++ dayO7/dom.js
@@ -0,0 +1,12 @@
+//const headers = document.getElementsByClassName('h1');
+const headers = document.querySelectorAll('h1');
+const list = document.getElementsByClassName('item');
+//const list = document.querySelectorAll('item');
+const paragraph = document.querySelectorAll('submit');
+
+
+
+console.log(headers);
+console.log(list);
+console.log(list.length);
+console.log (paragraph);
