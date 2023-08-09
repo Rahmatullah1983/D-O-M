0815d0e40011c509f575d092b88a5114152ad20f
Rahmatullah1983 <bugtirahmatullah@gmail.com>
2023-08-09 15:00:50 +0500

    today I have learnt events

--- day08/collections.js
+++ day08/collections.js
@@ -5,6 +5,7 @@ const newElement = document.createElement('li');
 newElement.classList.add("item")
 newElement.innerText = 'item 3'
 todoList.appendChild(newElement);
+
 const otherElement = document.createElement('h2');
 otherElement.classList.add("item");
 newElement.innerText="wow I created newElent";
---
+++ day09/event.js
@@ -0,0 +1,24 @@
+const button = document.querySelector("#submit");
+const todolist = document.querySelector('#todo');
+const todoNr =document.querySelector('.todoNr b');
+const main = document.querySelector('.main');
+
+//console.log(todoNr);
+//const items = todolist.children;
+
+//button.addEventListener("click", function(){
+  //  const newItem= document.createElement('li');
+  //  newItem.classList.add('item');
+  //  newItem.innerText = `item ${items.length + 1}`;
+  //  todolist.appendChild(newItem);
+    
+//todoNr.innerText=  items.length;
+//})
+
+button.addEventListener("click", function() {
+  main.style.color= "red";
+  main.style.fontSize = '50px';
+
+});
+
+
